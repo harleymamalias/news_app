3b511f1695d8fb5a03e341155d6ef47c4ef4ff84
Harley Mamalias <harleymamalias@gmail.com>
2023-10-27 03:38:38 +0800

    added bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:news_app/screens/home-screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int selectedIndex = 0;
+
+  void onItemTapped(int index) {
+    // print('Tapped index: $index');
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,7 +30,40 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Color(0xFFFCFCFC),
-        body: NewsAppHomeScreen(),
+        body: const NewsAppHomeScreen(),
+        bottomNavigationBar: BottomNavigationBar(
+          elevation: 0,
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.white,
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: selectedIndex == 0
+                  ? SvgPicture.asset('assets/home_selected_icon.svg')
+                  : SvgPicture.asset('assets/home_unselected_icon.svg'),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: selectedIndex == 1
+                  ? SvgPicture.asset('assets/bookmark_selected_icon.svg')
+                  : SvgPicture.asset('assets/bookmark_unselected_icon.svg'),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: selectedIndex == 2
+                  ? SvgPicture.asset('assets/notification_selected_icon.svg')
+                  : SvgPicture.asset('assets/notification_unselected_icon.svg'),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: selectedIndex == 3
+                  ? SvgPicture.asset('assets/profile_selected_icon.svg')
+                  : SvgPicture.asset('assets/profile_unselected_icon.svg'),
+              label: '',
+            ),
+          ],
+          currentIndex: selectedIndex,
+          onTap: onItemTapped,
+        ),
       ),
     );
   }
