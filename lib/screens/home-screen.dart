2a30dde165da32f09948a481d23cc2cc428399ce
Harley Mamalias <harleymamalias@gmail.com>
2023-10-26 22:39:47 +0800

    added initial assets and organize folders

---
+++ lib/screens/home-screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NewsAppHomeScreen extends StatefulWidget {
+  const NewsAppHomeScreen({super.key});
+
+  @override
+  State<NewsAppHomeScreen> createState() => _NewsAppHomeScreenState();
+}
+
+class _NewsAppHomeScreenState extends State<NewsAppHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
